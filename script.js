37bea765350cd89ea0723448da50a2fbb13175fc
oslifa <224970287+oslifa@users.noreply.github.com>
2025-10-24 18:24:03 +0100

    Create initial styling for calculator in CSS

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+let operand1;
+let operator;
+let operand2;
+
+function operate(operand1, operator, operand2) {
+  return operator(operand1, operand2);
+}
